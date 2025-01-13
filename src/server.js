49e180326b502ce392345e28b6a0efc1507cb792
ridwanr00 <rasyid.r00@gmail.com>
2025-01-13 14:18:11 +0700

    use rate limiter and compression

--- src/server.js
+++ src/server.js
@@ -1,16 +1,26 @@
 import express from "express";
-import userRoutes from './user.routes'
-import mainRoutes from './main.routes'
+import userRoutes from "./user.routes";
+import mainRoutes from "./main.routes";
 import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-app.use(helmet())
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  limit: 100, 
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
 
-app.use('/v1/', mainRoutes)
-app.use('/v1/user/', userRoutes)
+app.use(limiter);
+app.use(express.json());
+app.use(helmet());
+app.use(compression)
+
+app.use("/v1/", mainRoutes);
+app.use("/v1/user/", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
