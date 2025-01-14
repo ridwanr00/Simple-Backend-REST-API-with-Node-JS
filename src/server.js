a8cf5be0b43b0289c11eb865c22c98448cc31a19
ridwanr00 <rasyid.r00@gmail.com>
2025-01-14 16:07:37 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ import mainRoutes from "./main.routes";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import compression from "compression";
+import cors from 'cors'
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
@@ -17,6 +18,7 @@ app.use(limiter);
 app.use(express.json());
 app.use(helmet());
 app.use(compression());
+app.use(cors())
 
 app.use("/v1/", mainRoutes);
 app.use("/v1/user/", userRoutes);
